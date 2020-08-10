b7729e370694c5d8f5876006499fe5bc7138e322
Reg1ss <654052909@qq.com>
2020-08-10 02:10:05 +0100

    fix model parameters

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from pytorch_lightning import Trainer
-from model import poseNet, config
+from model import poseNet
 from pytorch_lightning.callbacks import EarlyStopping
 
-this_config = config.__config__
-inp_dim = this_config['inp_dim']
-oup_dim = this_config['oup_dim']
-net = poseNet.poseNet(inp_dim, oup_dim)
+
+model = poseNet.poseNet()
+net = model.load_from_checkpoint('/home/reg1s/PycharmProjects/pytorchLightning_StackedHourglass'
+                                                     '/checkpoint/test_hg1_02/lightning_logs/version_3/checkpoints/epoch=10.ckpt')
 
 early_stopping = EarlyStopping('val_loss')
 trainer = Trainer(gpus=1, default_root_dir='./checkpoint/test_hg1_02', early_stop_callback=early_stopping, log_gpu_memory=True)
-trainer.test(ckpt_path='/home/reg1s/PycharmProjects/pytorchLightning_StackedHourglass/checkpoint/test_hg1_02/lightning_logs/version_3/checkpoints/epoch=10.ckpt')
+trainer.test(net)
+#trainer.test(net)
--- train.py
+++ train.py
@@ -1,11 +1,9 @@
 from pytorch_lightning import Trainer
-from model import poseNet, config
+from model import poseNet
 from pytorch_lightning.callbacks import EarlyStopping
 
-this_config = config.__config__
-inp_dim = this_config['inp_dim']
-oup_dim = this_config['oup_dim']
-net = poseNet.poseNet(inp_dim, oup_dim)
+
+net = poseNet.poseNet()
 
 early_stopping = EarlyStopping('val_loss')
 trainer = Trainer(gpus=1, default_root_dir='./checkpoint/test_hg1_02', early_stop_callback=early_stopping, log_gpu_memory=True)
