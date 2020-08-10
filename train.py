bf64e4dc3253e3a33dc04ff2cff7c972241dcc19
Reg1ss <654052909@qq.com>
2020-08-10 01:33:52 +0100

    modify train.py

--- train.py
+++ train.py
@@ -3,11 +3,9 @@ from model import poseNet, config
 from pytorch_lightning.callbacks import EarlyStopping
 
 this_config = config.__config__
-nstack = this_config['nstack']
 inp_dim = this_config['inp_dim']
 oup_dim = this_config['oup_dim']
-batch_size = this_config['batch_size']
-net = poseNet.poseNet(nstack, inp_dim, oup_dim, batch_size)
+net = poseNet.poseNet(inp_dim, oup_dim)
 
 early_stopping = EarlyStopping('val_loss')
 trainer = Trainer(gpus=1, default_root_dir='./checkpoint/test_hg1_02', early_stop_callback=early_stopping, log_gpu_memory=True)
